799382d429a1a6c24984af4f9a4db9d24e52bdd9
Tyler <toreskey@gmail.com>
2020-10-06 13:15:27 -0600

    removed index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  return <div className="App"></div>;
+};
 
 export default App;
