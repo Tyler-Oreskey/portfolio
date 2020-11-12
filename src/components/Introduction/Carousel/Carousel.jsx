910831269aaf5c7ffed18861d2d7104ede0c369e
Tyler <toreskey@gmail.com>
2020-11-12 10:01:35 -0700

    added transition animation

--- src/components/Introduction/Carousel/Carousel.jsx
+++ src/components/Introduction/Carousel/Carousel.jsx
@@ -1,35 +1,44 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+// /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
-import { useSpring, animated, useTrail, useTransition } from "react-spring";
+import { animated, useTransition } from "react-spring";
 
 import classes from "./Carousel.module.css";
 
 const Carousel = () => {
-  const [carouselItems, setCarouselItems] = useState([]);
+  const [carouselItems] = useState([
+    { content: "yeetus 0", id: 0 },
+    { content: "fetus 1", id: 1 },
+    { content: "destroyer", id: 2 },
+    { content: "ddd", id: 3 },
+    { content: "sdsfddsf 2", id: 4 },
+  ]);
   const [slideTimer, setSlideTimer] = useState(5000);
   let [slideIndex, setSlideIndex] = useState(0);
 
-  useEffect(() => {
-    const allCarouselItems = getCarouselItems();
-    setCarouselItems(allCarouselItems);
-  }, []);
+  const fadingText = useTransition(
+    carouselItems[slideIndex],
+    (carouselItem) => carouselItem.id,
+    {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 },
+      config: { tension: 220, friction: 120 },
+    }
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
       setSlideIndex((slideIndex) => (slideIndex + 1) % carouselItems.length);
     }, slideTimer);
     return () => clearInterval(interval);
-  }, [slideIndex, carouselItems]);
+  }, [slideIndex, carouselItems, slideTimer]);
 
-  return <div className={classes.Carousel}>{carouselItems[slideIndex]}</div>;
+  const animatedItems = fadingText.map(({ item, props, key }) => (
+    <animated.div key={key} style={{ ...props, position: "absolute" }}>
+      <p>{item.content}</p>
+    </animated.div>
+  ));
+
+  return animatedItems;
 };
-
-const getCarouselItems = () => [
-  "yeetus 0",
-  "fetus 1",
-  "deleteus 2",
-  "ddd",
-  "sdsfddsf 2",
-];
-
 export default Carousel;
