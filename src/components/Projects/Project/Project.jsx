1a4964734110f42a42783f626f45aed54f914e62
Tyler <toreskey@gmail.com>
2020-11-16 11:46:14 -0700

    added react spring project card flip animation

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -1,26 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
 
 import classes from "./Project.module.css";
 
-class Project extends Component {
-  state = {
-    name: "",
-    description: "",
-  };
+const Project = (props) => {
+  const [flipped, setFlipped] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
 
-  componentDidMount() {
-    const { name, description } = this.props.location.state;
-    this.setState({ name, description });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Title of course is: {this.state.name}</h1>
-        <p>Description is: {this.state.description}</p>
+  return (
+    <div onClick={() => setFlipped(!flipped)} className={classes.ProjectCard}>
+      <div className={classes.ProjectCardInner}>
+        <animated.div
+          className={`card ${classes.Front}`}
+          style={{ opacity: opacity.interpolate((o) => 1 - o), transform }}
+        >
+          <img src={props.image} alt="img" />
+        </animated.div>
+        <animated.div
+          className={`card ${classes.Back}`}
+          style={{
+            opacity,
+            transform: transform.interpolate((t) => `${t} rotateX(180deg)`),
+          }}
+        >
+          <p>{props.description}</p>
+        </animated.div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Project;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import Project from "./Project/Project";
 
 import classes from "./Projects.module.css";
 
@@ -14,16 +14,22 @@ class Projects extends Component {
         id: 1,
         name: "Fishtopia",
         description: "Fishtopia description...",
+        image:
+          "https://www.greaterseattleonthecheap.com/wordpress/wp-content/uploads/2020/05/Fly-fishing-with-Emerald-Water-Anglers.jpg",
       },
       {
         id: 2,
         name: "Good Samaratin",
         description: "Good Samaratin description...",
+        image:
+          "https://www.benzinga.com/files/images/story/2012/life-extension-technology-and-cannabis-formulations.jpg",
       },
       {
         id: 3,
         name: "Hackateam",
         description: "Hackateam description...",
+        image:
+          "https://www.phocuswire.com/uploadedimages/uploads/2018/01/hedna-hackathon.jpg?width=800&height=400&scale=both&mode=crop",
       },
     ];
     this.setState({ projects: projectsData });
@@ -31,26 +37,16 @@ class Projects extends Component {
   render() {
     const projects = this.state.projects.map((project) => {
       return (
-        <Link
+        <Project
           key={project.id}
-          to={{
-            pathname: `${this.props.match.url}/${project.id}`,
-            state: {
-              name: project.name,
-              description: project.description,
-            },
-          }}
-        >
-          <article className={classes.Project}>{project.name}</article>
-        </Link>
+          name={project.name}
+          description={project.description}
+          image={project.image}
+        ></Project>
       );
     });
 
-    return (
-      <div className={classes.Projects}>
-        <ul>{projects}</ul>
-      </div>
-    );
+    return <div className={classes.Projects}>{projects}</div>;
   }
 }
 
