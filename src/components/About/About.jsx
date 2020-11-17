9a7e93948b05a51017e7e4a52081a972cd2578cb
Tyler <toreskey@gmail.com>
2020-11-17 14:00:34 -0700

    moved introduction over to about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import Carousel from "./Carousel/Carousel";
+import SocialIcons from "./SocialIcons/SocialIcons";
 
-import Introduction from "../Introduction/Introduction";
-
+import code from "../../assets/images/icons/code.png";
 import classes from "./About.module.css";
 
 const About = () => {
   return (
-    <div>
-      <Introduction />
+    <div className={`${classes.About} container`}>
+      <div className="row">
+        <div className="col">
+          <h1>Hello, I'm</h1>
+          <h2>Tyler Oreskey</h2>
+          <h3>
+            <img src={code} alt="</>" />
+            Full Stack Web Developer
+          </h3>
+          <SocialIcons />
+          <Carousel />
+        </div>
+        <div className="col">
+          <h1>Picture Here!!!</h1>
+        </div>
+      </div>
     </div>
   );
 };
