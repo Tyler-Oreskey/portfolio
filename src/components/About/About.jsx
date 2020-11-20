6bbdd3f5425738ecb9176320eb2e42c8513b1408
tyler-oreskey <toreskey@gmail.com>
2020-11-20 11:35:03 -0700

    added photo of myself and styled about page to fit all devices

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,22 +5,28 @@ import SocialIcons from "./SocialIcons/SocialIcons";
 import code from "../../assets/images/icons/code.png";
 import classes from "./About.module.css";
 
+import about from "../../assets/images/me/about.jpg";
+
 const About = () => {
   return (
     <div className={`${classes.About} container`}>
       <div className="row">
         <div className="col-12 col-md-7">
-          <h1>Hello, I'm</h1>
-          <h2>Tyler Oreskey</h2>
-          <h3>
-            <img src={code} alt="</>" />
+          <div className={classes.Col1}>
+            <h1>Hello, I'm</h1>
+            <h2>Tyler Oreskey</h2>
+            <h3>
+              <img src={code} alt="</>" />
             Full Stack Web Developer
           </h3>
-          <SocialIcons />
-          <Carousel />
+            <SocialIcons />
+            <Carousel />
+          </div>
         </div>
         <div className="col-12 order-first order-md-0 col-md-5">
-          <h1>Picture Here!!!</h1>
+          <div className={classes.Col2}>
+            <img className={classes.Me} src={about} alt="me" />
+          </div>
         </div>
       </div>
     </div>
