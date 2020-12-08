abe8ada0ff85aa5b869d2e1d3c8ad0a590ddb24e
tyler-oreskey <toreskey@gmail.com>
2020-12-08 10:29:51 -0700

    added front end caching for portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Carousel from "./Carousel/Carousel";
 import SocialIcons from "./SocialIcons/SocialIcons";
+import RequestHandler from '../../hoc/RequestHandler/RequestHandler';
+import axios from '../../axios';
+import about from "../../assets/images/me/about.jpg";
 
 import code from "../../assets/images/icons/code.png";
 import classes from "./About.module.css";
 
-import about from "../../assets/images/me/about.jpg";
-
 const About = () => {
+
+  const downloadResume = async () => {
+    const res = await axios.get('/resume/getPDF');
+    const downloadLink = document.createElement("a");
+    const filename = "resume.pdf";
+    downloadLink.href = res.data.blob;
+    downloadLink.download = filename;
+    downloadLink.click();
+  };
+
   return (
     <div className={`${classes.About} container`}>
       <div className="row">
@@ -20,6 +31,10 @@ const About = () => {
             Full Stack Web Developer
             <SocialIcons />
             </h3>
+            <button
+              onClick={() => downloadResume()}>
+              Download Resume
+            </button>
             <Carousel />
           </div>
         </div>
@@ -33,4 +48,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default RequestHandler(About, axios);
