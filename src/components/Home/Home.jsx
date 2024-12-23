91424da7f85b45d8bb78bcfffb3f1d90e178aa27
tyler-oreskey <toreskey@gmail.com>
2024-12-23 00:29:03 +0000

    refactored home component.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,68 @@
 import React from "react";
 
-import Greeting from "./Greeting/Greeting";
-import Occupation from "./Occupation/Occupation";
 import ButtonHoverEffectWithRightArrow from "../../UI/Buttons/ButtonHoverEffectWithRightArrow/ButtonHoverEffectWithRightArrow";
-import Resume from "./Resume/Resume";
+import ButtonHoverEffect from "../../UI/Buttons/ButtonHoverEffect/ButtonHoverEffect";
+import resume from "../../assets/files/resume.pdf";
+import codeIcon from "../../assets/images/icons/code.png";
 
 import classes from "./Home.module.css";
 
-const Home = (props) => (
-  <div className={`container-fluid ${classes.Home}`} ref={props.reference}>
-    <Greeting />
-    <Occupation />
-    <div className={classes.HomeButtonRows}>
-      <div className={classes.HomeButton}>
-        <ButtonHoverEffectWithRightArrow
-          clicked={props.click}
-          buttonText="View My Work"
-        />
-      </div>
-      <div className={classes.HomeButton}>
-        <Resume />
+export default function Home(props) {
+  return (
+    <div className={`container-fluid ${classes.Home}`} ref={props.reference}>
+      <Greeting />
+      <Occupation />
+      <div className={classes.HomeButtonRows}>
+        <div className={classes.HomeButton}>
+          <ButtonHoverEffectWithRightArrow
+            clicked={props.click}
+            buttonText="View My Work"
+          />
+        </div>
+        <div className={classes.HomeButton}>
+          <Resume />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
-export default Home;
+function Greeting() {
+  return (
+    <div className={classes.Greeting}>
+      <p>Hello, I'm </p>
+      <span>Tyler Oreskey</span>
+    </div>
+  );
+}
+
+function Occupation() {
+  return (
+    <div className={classes.Occupation}>
+      <p>
+        <img src={codeIcon} alt="</>" /> Software Engineer
+      </p>
+    </div>
+  );
+}
+
+function Resume() {
+  const initiateDownload = async () => {
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = "Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div>
+      <ButtonHoverEffect
+        clicked={initiateDownload}
+        buttonText="Resume"
+        textColor="white"
+      />
+    </div>
+  );
+}
