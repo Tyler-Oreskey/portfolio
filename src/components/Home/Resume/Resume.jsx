2eb707cc05356aa460bbdafae6c98e895b5dec3c
tyler-oreskey <toreskey@gmail.com>
2024-12-23 00:03:53 +0000

    refactored resume

--- src/components/Home/Resume/Resume.jsx
+++ src/components/Home/Resume/Resume.jsx
@@ -1,48 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 
-import RequestHandler from "../../../hoc/RequestHandler/RequestHandler";
-import axios from "../../../axios";
-import Spinner from "../../../UI/Spinner/Spinner";
 import ButtonHoverEffect from "../../../UI/Buttons/ButtonHoverEffect/ButtonHoverEffect";
+import resume from "../../../assets/files/resume.pdf";
 
-const Resume = () => {
-  const [loading, setLoading] = useState(false);
-
+export default function Resume() {
   const initiateDownload = async () => {
-    try {
-      setLoading(true);
-      const res = await axios.resume.get("/resume/getPDF");
-      const arr = new Uint8Array(res.data.resumeBuffer.buffer.data);
-      const blob = new Blob([arr], { type: "application/pdf" });
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "resume.pdf";
-      link.click();
-      setTimeout(() => {
-        // firefox and safari browsers
-        window.URL.revokeObjectURL(url);
-      }, 100);
-    } catch (error) {
-      // handled by request handler hoc
-    } finally {
-      setLoading(false);
-    }
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = "Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
-  let downloadResumeButton = (
-    <ButtonHoverEffect
-      clicked={initiateDownload}
-      buttonText="Resume"
-      textColor="white"
-    />
+  return (
+    <div>
+      <ButtonHoverEffect
+        clicked={initiateDownload}
+        buttonText="Resume"
+        textColor="white"
+      />
+    </div>
   );
-
-  if (loading) {
-    downloadResumeButton = <Spinner size="medium" />;
-  }
-
-  return <div>{downloadResumeButton}</div>;
-};
-
-export default RequestHandler(Resume, axios.resume);
+}
