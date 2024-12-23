95d2e3fc09c0d6012cf53d4a430543449472b184
tyler-oreskey <toreskey@gmail.com>
2024-12-23 05:00:34 +0000

    reworked projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,84 +1,130 @@
-import React, { Component } from "react";
-
-import ProjectNav from "./ProjectNav/ProjectNav";
-import ProjectGroup from "./ProjectGroup/ProjectGroup";
-import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+import React, { useState, useEffect } from "react";
+import Project from "./ProjectGroup/Project/Project";
 import Modal from "../../UI/Modal/Modal";
-import ProjectModalContent from "./ProjectModalContent/ProjectModalContent";
 import projectItems from "../../displayData/projects";
 
 import classes from "./Projects.module.css";
 
-class Projects extends Component {
-  state = {
-    showModal: false,
-    modalContent: null,
-    allProjects: {},
-    navIndex: 0,
-    selectedProjectGroup: [],
-    defaultProjectGroup: "ALL",
-  };
+const Projects = ({ reference }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const [allProjects, setAllProjects] = useState({});
+  const [navIndex, setNavIndex] = useState(0);
+  const [selectedProjectGroup, setSelectedProjectGroup] = useState([]);
+  const defaultProjectGroup = "ALL";
 
-  componentDidMount() {
-    const allProjects = projectItems;
-
-    for (const key in allProjects) {
-      if (key !== this.state.defaultProjectGroup) {
-        projectItems[key].map((project) => {
-          allProjects[this.state.defaultProjectGroup].push(project);
+  useEffect(() => {
+    const allProjectsData = JSON.parse(JSON.stringify(projectItems));
+  
+    allProjectsData[defaultProjectGroup] = [];
+  
+    for (const key in allProjectsData) {
+      if (key !== defaultProjectGroup) {
+        allProjectsData[key].forEach((project) => {
+          allProjectsData[defaultProjectGroup].push(project);
         });
       }
     }
+  
+    setAllProjects(allProjectsData);
+    setSelectedProjectGroup(allProjectsData[defaultProjectGroup]);
+  }, [defaultProjectGroup]);
+  
 
-    this.setState({
-      allProjects: allProjects,
-      selectedProjectGroup: allProjects[this.state.defaultProjectGroup],
-    });
-  }
-
-  handleClickShowModal = (modalContent) => {
-    this.setState({
-      modalContent,
-      showModal: true,
-    });
+  const handleClickShowModal = (content) => {
+    setModalContent(content);
+    setShowModal(true);
   };
 
-  handleCloseModal = () => this.setState({ showModal: false });
+  const handleCloseModal = () => setShowModal(false);
 
-  handleChangeProjectGroup = (projectGroup, navIndex) => {
-    this.setState({
-      selectedProjectGroup: this.state.allProjects[projectGroup],
-      navIndex,
-    });
+  const handleChangeProjectGroup = (projectGroup, navIndex) => {
+    setSelectedProjectGroup(allProjects[projectGroup]);
+    setNavIndex(navIndex);
   };
 
-  render() {
+  const ProjectNav = () => {
+    const activeStyle = {
+      color: "white",
+      backgroundColor: "#dc143c",
+    };
+
+    const projectGroupItems = Object.keys(allProjects).map((group, idx) => (
+      <li
+        key={idx}
+        style={navIndex === idx ? activeStyle : null}
+        onClick={() => handleChangeProjectGroup(group, idx)}
+      >
+        {group}
+      </li>
+    ));
+
     return (
-      <Auxiliary>
-        <div className={`${classes.Projects}`} ref={this.props.reference}>
-          <h1>PROJECTS</h1>
-          <div className="container">
-            <div className="row">
-              <ProjectNav
-                navIndex={this.state.navIndex}
-                allProjects={this.state.allProjects}
-                handleChangeProjectGroup={this.handleChangeProjectGroup}
-              />
-            </div>
-            <div className="row">
-              <ProjectGroup
-                projectGroup={this.state.selectedProjectGroup}
-                clickShowModal={this.handleClickShowModal}
-              />
-            </div>
+      <div className={classes.ProjectNav}>
+        <ul>{projectGroupItems}</ul>
+      </div>
+    );
+  };
+
+  const ProjectGroup = () => {
+    return selectedProjectGroup.map((project, idx) => {
+      return (
+        <div className={`${classes.Project} col-12 col-md-6 col-lg-4`} key={idx}>
+          <Project project={project} clickShowModal={handleClickShowModal} />
+        </div>
+      );
+    });
+  };
+
+  const ProjectModalContent = () =>
+    modalContent !== null ? (
+      <div className={classes.ProjectModalContent}>
+        <div className={classes.AnimatedProjectBorder}></div>
+        <div className={classes.ProjectInfo}>
+          <h1>{modalContent.name}</h1>
+          <p>{modalContent.description}</p>
+          <p>
+            <span>Technologies Used: </span>
+            {modalContent.techs}
+          </p>
+        </div>
+        <div className={classes.ProjectLinks}>
+            <a
+              href={modalContent.projectLinks.frontend}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              FRONTEND
+            </a>
+            <a
+              href={modalContent.projectLinks.backend}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BACKEND
+            </a>
+        </div>
+      </div>
+    ) : null;
+
+  return (
+    <>
+      <div className={`${classes.Projects}`} ref={reference}>
+        <h1>PROJECTS</h1>
+        <div className="container">
+          <div className="row">
+            <ProjectNav />
+          </div>
+          <div className="row">
+            <ProjectGroup />
           </div>
         </div>
-        <Modal show={this.state.showModal} closeModal={this.handleCloseModal}>
-          <ProjectModalContent modalContent={this.state.modalContent} />
-        </Modal>
-      </Auxiliary>
-    );
-  }
-}
+      </div>
+      <Modal show={showModal} closeModal={handleCloseModal}>
+        <ProjectModalContent />
+      </Modal>
+    </>
+  );
+};
 
 export default Projects;
